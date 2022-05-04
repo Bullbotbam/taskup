6bca2b087e0cfb1437f73fc02446ca99e7a3fcc5
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2022-05-04 15:31:08 -0500

    add event default

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const buttonEl = window.document.querySelector('#save-task');
 const tasksToDoEl = document.querySelector('#tasks-to-do');
-const createTaskHandler = function () {
+const createTaskHandler = function (e) {
+	e.preventDefault();
 	let listItemEl = document.createElement('li');
 	listItemEl.className = 'task-item';
 	listItemEl.textContent = 'This is yet another task.';
